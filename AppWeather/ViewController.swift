2d34706bf0596a5684a841101ba083e272b001ac
Marc Watier <marcwatier@marcs-mac.home>
2016-10-14 11:02:09 +0200

    Initial Commit

---
+++ AppWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppWeather
+//
+//  Created by Marc Watier on 14/10/2016.
+//  Copyright © 2016 Marc Watier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
